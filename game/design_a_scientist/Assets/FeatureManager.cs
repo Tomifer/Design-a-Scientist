794695f6b06320563f7ef8ba4cd26f99476274f6
tjbatche <tjbatche@ncsu.edu>
2020-02-23 16:34:32 -0500

    Updated Files

---
+++ game/design_a_scientist/Assets/FeatureManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class FeatureManager : MonoBehaviour
+{
+
+    public List<Feature> features;
+    public int currFeature;
+
+    void OnEnable()
+    {
+        LoadFeatures();    
+    }
+
+    void OnDisable()
+    {
+        SaveFeatures();
+    }
+
+    void LoadFeatures()
+    {
+        features = new List<Feature>();
+        features.Add(new Feature("Hair", GameObject.Find("Hair").GetComponent<SpriteRenderer>()));
+    }
+
+    void SaveFeatures()
+    {
+
+    }
+
+    public void SetCurrent(int index)
+    {
+        if (features == null)
+            return;
+        currFeature = index;
+    }
+
+    public void NextChoice()
+    {
+        if (features == null)
+            return;
+        features[currFeature].currIndex++;
+    }
+
+    public void PreviousChoice()
+    {
+        if (features == null)
+            return;
+        features[currFeature].currIndex--;
+    }
+}
+
+[System.Serializable]
+public class Feature
+{
+    public string ID;
+    public int currIndex;
+    public Sprite[] choices;
+    public SpriteRenderer renderer;
+
+    public Feature(string id, SpriteRenderer rend)
+    {
+        ID = id;
+        renderer = rend;
+        UpdateFeature();
+    }
+
+    public void UpdateFeature()
+    {
+        choices = Resources.LoadAll<Sprite>("Textures/" + ID);
+
+        if (choices == null || renderer == null)
+            return;
+
+        if (currIndex < 0)
+            currIndex = choices.Length - 1;
+        if (currIndex >= choices.Length)
+            currIndex = 0;
+
+        renderer.sprite = choices[currIndex];
+    }
+}
---
+++ game/design_a_scientist/Assets/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform btnPrefab;
+
+    private FeatureManager manager;
+    private Text descText;
+    private List<Button> buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        manager = FindObjectOfType<FeatureManager>();
+       
+        descText = GameObject.Find("Description").GetComponent<Text>();
+        GameObject.Find("Previous").GetComponent<Button>().onClick.AddListener(() => manager.PreviousChoice());
+        GameObject.Find("Next").GetComponent<Button>().onClick.AddListener(() => manager.NextChoice());
+        Debug.Log("Running Start...");
+        InitializFeatureButtons();
+    }
+
+    void InitializFeatureButtons()
+    {
+        buttons = new List<Button>();
+
+        float height = btnPrefab.rect.height;
+        float width = btnPrefab.rect.width;
+        for(int i = 0; i < 2; i++)
+        {
+            RectTransform temp = Instantiate<RectTransform>(btnPrefab);
+            temp.name = i.ToString();
+            temp.SetParent(GameObject.Find("Features").GetComponent<RectTransform>());
+            temp.localScale = new Vector3(1, 1, 1);
+            temp.localPosition = new Vector3(0, 0, 0);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, i * height, height);
+
+            Button b = temp.GetComponent<Button>();
+            b.onClick.AddListener(() => manager.SetCurrent(int.Parse(temp.name)));
+            buttons.Add(b);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        descText.text = manager.features[manager.currFeature].ID + " #" + (manager.features[manager.currFeature].currIndex + 1);
+    }
+}
