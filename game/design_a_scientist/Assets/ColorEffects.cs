3eea1d6406730dbab0c1d0d28491296d74c4f3e3
ajbulthu <ajbulthu@ncsu.edu>
2020-05-20 20:10:11 -0400

    Added a new method of messing with colors
    Added a new method of messing with colors

---
+++ game/design_a_scientist/Assets/ColorEffects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorEffects : MonoBehaviour
+{
+    Texture2D mColorSwapTex;
+    public Color newColor;
+    SpriteRenderer mSpriteRenderer;
+    Color[] mSpriteColors;
+
+    void Start()
+    {
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+        InitColorSwapTex();
+    }
+
+    public void InitColorSwapTex()
+    {
+        Texture2D colorSwapTex = new Texture2D(256, 1, TextureFormat.RGBA32, false, false);
+        colorSwapTex.filterMode = FilterMode.Point;
+    
+        for (int i = 0; i < colorSwapTex.width; ++i)
+            colorSwapTex.SetPixel(i, 0, new Color(0.0f, 0.0f, 0.0f, 0.0f));
+    
+        colorSwapTex.Apply();
+    
+        mSpriteRenderer.material.SetTexture("_SwapTex", colorSwapTex);
+    
+        mSpriteColors = new Color[colorSwapTex.width];
+        mColorSwapTex = colorSwapTex;
+    }
+
+    public void SwapColor(int index, Color color)
+    {
+        mSpriteColors[(int)index] = color;
+        mColorSwapTex.SetPixel((int)index, 0, color);
+    }
+
+    public void go()
+    {
+        SwapColor(51, newColor);
+        mColorSwapTex.Apply();
+    }
+}
